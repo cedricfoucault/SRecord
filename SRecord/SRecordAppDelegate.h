4f70deed0f536fdb7209c09be94bc59ba08d3287
Cédric Foucault <cedric.foucault@gmail.com>
2013-05-26 19:58:50 -0700

    Initial Commit

---
+++ SRecord/SRecordAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SRecordAppDelegate.h
+//  SRecord
+//
+//  Created by Cédric Foucault on 5/26/13.
+//  Copyright (c) 2013 Cédric Foucault. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SRecordAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SRecord/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SRecord
+//
+//  Created by Cédric Foucault on 5/26/13.
+//  Copyright (c) 2013 Cédric Foucault. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SRecordAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SRecordAppDelegate class]));
+    }
+}
